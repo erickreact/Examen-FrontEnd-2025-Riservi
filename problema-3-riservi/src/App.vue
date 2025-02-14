691ba18f6bc881c9602df19b002754f624291932
erickreact <erick.transoro@gmail.com>
2025-02-14 15:17:40 -0500

    Codigo CSS para el archivo html
    Se agrega el codigo CSS de acuerdo a las especificaciones en el documento Examen Front-end 2025.docx

--- problema-3-riservi/src/App.vue
+++ problema-3-riservi/src/App.vue
@@ -1,3 +1,34 @@
+<template>
+<html>
+	<head>
+	</head>
+	<body>
+		<div id="contenedor">
+			<div class="bloque1">
+				<p>Este es el Bloque 1 dentro del contenedor.</p>
+			</div>
+			<div class="bloque2">
+				<p>Este es el contenido del Bloque 2 dentro del contenedor, este contiene mas texto, por eso debe ser mas largo.</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="bloque1">
+			<p>Este es el Bloque 1 fuera del contenedor.</p>
+		</div>
+		<div class="bloque2">
+			<p>Este es el Bloque 2 fuera del contenedor.</p>
+		</div>
+		<div class="v-container">
+
+		</div>
+		<div class="btn">
+			Generar
+		</div>
+		<div class="output">
+		</div>
+	</body>
+</html>
+</template>
 <script>
 export default {
 
@@ -20,41 +51,62 @@ export default {
 	}
 }
 </script>
-<template>
-<html>
-	<head>
-	</head>
-	<body>
-		<div id="contenedor">
-			<div class="bloque1">
-				<p>Este es el Bloque 1 dentro del contenedor.</p>
-			</div>
-			<div class="bloque2">
-				<p>Este es el contenido del Bloque 2 dentro del contenedor, este contiene mas texto, por eso debe ser mas largo.</p>
-			</div>
-		</div>
-		<div class="bloque1">
-			<p>Este es el Bloque 1 fuera del contenedor.</p>
-		</div>
-		<div class="bloque2">
-			<p>Este es el Bloque 2 fuera del contenedor.</p>
-		</div>
-		<div class="v-container">
-
-		</div>
-		<div class="btn">
-			Generar
-		</div>
-		<div class="output">
-		</div>
-	</body>
-</html>
-</template>
 <style type="text/css">
 
-.clear {
-	clear:both;
+body {
+    text-align: center;
 }
-	
+
+#contenedor {
+    width: 600px;
+    margin: 0 auto;
+    padding: 10px;
+    border: 1px dashed gray;
+    margin-bottom: 10px;
+}
+
+#contenedor .bloque1 {
+    width: 100px;
+    margin-top: 70px;
+    float: left;
+	margin: 5px;
+    background-color: red;
+    color: #F5F5DC;
+}
+
+#contenedor .bloque2 {
+    width: 100px;
+    float: left;
+    margin: 5px;
+    background-color: yellow;
+    color: black;
+}
+
+#contenedor .bloque1 p {
+    font-style: italic;
+}
+
+.bloque2 p {
+  font-weight: bold;
+}
+
+.bloque1 {
+    background-color: red;
+    color: #F5F5DC;
+}
+
+.bloque2 {
+    background-color: yellow;
+    color: black;
+}
+
+p {
+  padding: 10px;
+}
+
+.clear {
+    clear: both;
+}
+
 </style>
 
